b076174a6ef6d82bba583b6af4a345bef32e1357
Chien <cheng.cit@gmail.com>
2017-10-08 10:58:38 +0700

    update item left menu

--- app/src/main/java/node/com/enjoydanang/ui/activity/main/MainActivity.java
+++ app/src/main/java/node/com/enjoydanang/ui/activity/main/MainActivity.java
@@ -75,45 +75,23 @@ public class MainActivity extends MvpActivity<MainPresenter> implements MainView
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_personal) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_change_pass) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_intro) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_setting) {
 
         }
 
